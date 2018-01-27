b5ced6b6fbf2b1837a10d083f3318cca0ba0b311
Silent Accordion <bradford.knowlton@umconnect.umt.edu>
2018-01-27 15:03:11 -0700

    working reloadable server

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # pyserver
+
+
+https://pypi.python.org/pypi/pydemon/0.1.1
+
+Monitor for any changes in your Project
+
+##How to install
+
+$ sudo pip install pydemon
+
+##How to use
+
+$ pydemon 'bash script'
+or
+$ pydemon script.sh
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 """
 Very simple HTTP server in python.
 Usage::
@@ -10,8 +10,22 @@ Send a HEAD request::
 Send a POST request::
     curl -d "foo=bar&bin=baz" http://localhost
 """
+
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
+import signal
+import time
+import urllib2
+
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
+    print "Received Shutdown Signal"    
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -21,7 +35,7 @@ class S(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+        self.wfile.write("<html><body><h1>These aren't the ports you are looking for!!</h1></body></html>")
 
     def do_HEAD(self):
         self._set_headers()
@@ -30,16 +44,22 @@ class S(BaseHTTPRequestHandler):
         # Doesn't do anything with posted data
         self._set_headers()
         self.wfile.write("<html><body><h1>POST!</h1></body></html>")
-        
+
+
 def run(server_class=HTTPServer, handler_class=S, port=80):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print 'Starting httpd...'
-    httpd.serve_forever()
-
+    # httpd.serve_forever()
+    killer = GracefulKiller()
+    while True:
+        httpd.handle_request()
+        if killer.kill_now:
+            break
+    # requests.get('http://localhost:'+server_address)
+    
 if __name__ == "__main__":
     from sys import argv
-
     if len(argv) == 2:
         run(port=int(argv[1]))
     else:
@@ -47,4 +67,5 @@ if __name__ == "__main__":
 
 
 # Credits
-# https://gist.github.com/bradmontgomery/2219997        
+# https://gist.github.com/bradmontgomery/2219997
+# https://gist.github.com/becxer/30fc93bc56c8ab006a17
